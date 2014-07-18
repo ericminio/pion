20eb954dc0bc8b8ddc21c2aefd735115daeea1ac
ericminio <eric.mignot@gmail.com>
2014-07-18 09:02:59 -0400

    We can detect a duplication in a single file

--- app/features/duplicate.lines/duplication.detection.spec.js
+++ app/features/duplicate.lines/duplication.detection.spec.js
@@ -1,24 +1,28 @@
 var fs = require('fs');
 var detectDuplicateCodeInFolder = require('./lib/detect.duplicate.code.in.folder');
+var array = require('../utils/lib/array.utils');
 
 describe('Duplication detection', function() {
 
 	var folder = 'test-data';
 	
 	beforeEach(function() {	
-		if (!fs.existsSync(folder)) fs.mkdirSync(folder);			
+		if (!fs.existsSync(folder)) fs.mkdirSync(folder);
+		var files = fs.readdirSync(folder);
+		array.forEach(files, function(file) {
+			fs.unlinkSync(folder + '/' + file);
+		});
 	});
 	
-	it('works with one line duplicated in two files', function() {
-		var content = 'hello world';
-		fs.writeFileSync(folder + '/first-file', content);
-		fs.writeFileSync(folder + '/second-file', content);
+	it('works with one line duplicated in the same file', function() {
+		var content = 'hello world\nhello world';
+		fs.writeFileSync(folder + '/one-file', content);
 		var duplication = detectDuplicateCodeInFolder(folder);
 		
 		expect(duplication).toEqual({
 			line: 'hello world',
-			left:  { file: 'first-file', line: 1 },
-			right: { file: 'second-file', line: 1 },
+			left:  { file: 'one-file', line: 1 },
+			right: { file: 'one-file', line: 2 },
 		});
 	});
 });
