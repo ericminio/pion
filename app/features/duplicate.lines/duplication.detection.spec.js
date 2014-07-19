e3d74955565133ead49d3135975be36da01c8159
ericminio <eric.mignot@gmail.com>
2014-07-18 23:49:47 -0400

    We can detect one line duplicated three times in one file

--- app/features/duplicate.lines/duplication.detection.spec.js
+++ app/features/duplicate.lines/duplication.detection.spec.js
@@ -6,36 +6,35 @@ var inFolder = require('../utils/lib/directory.file.provider');
 describe('Pion', function() {
 
 	var folder = 'test-data/';
+	var onefile = 'one-file';
 	
 	beforeEach(function() {	
 		clean.folder(folder);		
 	});
 	
 	it('can detect one line duplicated two times in one file', function() {
-		var filename = 'one-file';
 		var content = 'first line\nfirst duplication\nfirst duplication';
-		havingInFolder(folder).theFileWithName(filename).withContent(content);
+		havingInFolder(folder).theFileWithName(onefile).withContent(content);
 		
 		expect(duplications.inFiles(inFolder(folder))).toEqual([{
 			line: 'first duplication',
 			occurences: [
-				{ file: folder+filename, lineIndex: 1 },
-				{ file: folder+filename, lineIndex: 2 }
+				{ file: folder+onefile, lineIndex: 1 },
+				{ file: folder+onefile, lineIndex: 2 }
 			]
 		}]);
 	});
 
 	it('can detect one line duplicated three times in one file', function() {
-		var filename = 'one-file';
 		var content = 'first line\nfirst duplication\nfirst duplication\nfirst duplication';
-		havingInFolder(folder).theFileWithName(filename).withContent(content);
+		havingInFolder(folder).theFileWithName(onefile).withContent(content);
 		
 		expect(duplications.inFiles(inFolder(folder))).toEqual([{
 			line: 'first duplication',
 			occurences: [
-				{ file: folder+filename, lineIndex: 1 },
-				{ file: folder+filename, lineIndex: 2 },
-				{ file: folder+filename, lineIndex: 3 }
+				{ file: folder+onefile, lineIndex: 1 },
+				{ file: folder+onefile, lineIndex: 2 },
+				{ file: folder+onefile, lineIndex: 3 }
 			]
 		}]);
 	});
--- app/features/duplicate.lines/lib/pion.js
+++ app/features/duplicate.lines/lib/pion.js
@@ -1,31 +1,40 @@
 var fs = require('fs');
 var array = require('../../utils/lib/array.utils');
 
-var contain = function(line, duplications) {
-	var found = false;
-	array.forEach(duplications, function(duplication) {
-		if (duplication.line == line) {
-			found = true;
-			return;
-		}
+var containsLine = function(line, duplications) {
+	return array.first(duplications, function(duplication) {
+		return duplication.line == line;
 	});
-	return found;
+};
+
+var containsIndex = function(index, duplication) {
+	return array.first(duplication.occurences, function(occurence) {
+		return occurence.lineIndex == index;		
+	});
 };
 
 var duplications = function(leftFile, rightFile, fileProvider) {
 	var duplications = [];
 	var leftLines = fileProvider.contentOf(leftFile).split('\n');
 	var rightLines = fileProvider.contentOf(rightFile).split('\n');		
+
 	array.forEach(leftLines, function(leftLine, leftIndex) {
 		array.forEach(rightLines, function(rightLine, rightIndex) {
-			if (leftLine == rightLine && leftIndex !== rightIndex && ! contain(leftLine, duplications) ) {
-				duplications.push({
-					line: leftLine,
-					occurences: [
-						{ file:leftFile, lineIndex: leftIndex },
-						{ file:rightFile, lineIndex: rightIndex}
-					]
-				});
+			if (leftLine == rightLine && leftIndex !== rightIndex) {
+				var duplication = containsLine(leftLine, duplications);
+				if (! duplication) {
+					duplications.push({
+						line: leftLine,
+						occurences: [
+							{ file:leftFile, lineIndex: leftIndex },
+							{ file:rightFile, lineIndex: rightIndex}
+						]
+					});
+				} else {
+					if (!containsIndex(rightIndex, duplication)) {
+						duplication.occurences.push({ file:rightFile, lineIndex: rightIndex});
+					}
+				}
 			}
 		});
 	});
