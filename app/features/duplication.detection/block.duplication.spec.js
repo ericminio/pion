65e113d03c30fa3d9487304fc209b26e72ea5820
ericminio <eric.mignot@gmail.com>
2014-07-19 23:45:53 -0400

    Pretend that Pion can detect a duplicated block in one file

---
+++ app/features/duplication.detection/block.duplication.spec.js
@@ -0,0 +1,27 @@
+var clean = require('../utils/lib/clean');
+var havingInFolder = require('../utils/lib/having.in.folder');
+var duplications = require('./lib/pion');
+var inFolder = require('../utils/lib/directory.file.provider');
+
+describe('Pion', function() {
+
+	var folder = 'test-data/';
+	var onefile = 'one-file';
+	
+	beforeEach(function() {	
+		clean.folder(folder);	
+	});
+	
+	it('can detect a duplicated block in one file', function() {
+		var content = 'first item\nsecond item\nfirst item\nsecond item';
+		havingInFolder(folder).theFileWithName(onefile).withContent(content);
+		
+		expect(duplications.inFiles(inFolder(folder))).toEqual([{
+			lines: ['first item', 'second item'],
+			occurences: [
+				{ file: folder+onefile, lineIndex: 0 },
+				{ file: folder+onefile, lineIndex: 2 }
+			]
+		}]);
+	});
+});
