4e8416b44608625e06531aeb5db71ec24e573a10
ericminio <eric.mignot@gmail.com>
2014-07-21 23:48:58 -0400

    Pion can detect a duplicated block of two lines in one file :)

--- app/features/duplication.detection/lib/pion.blocks.js
+++ app/features/duplication.detection/lib/pion.blocks.js
@@ -1,15 +1,40 @@
 var array = require('../../utils/lib/array.utils');
 var occurence = require('./occurence');
 var line = require('./line');
+var linesDuplications = require('./pion.lines');
 
 module.exports = {
 	
 	inFiles: function(fileProvider) {
-		var filenames = fileProvider.files();
+		var duplicatedLines = linesDuplications.inFiles(fileProvider);
+		var duplicatedBlocks = [];
 		
-		var duplications = [];
+		array.forEach(duplicatedLines, function(left) {
+			array.forEach(duplicatedLines, function(right) {
+				if (left != right) {
+					log(left.lines + ' ' + right.lines);
+					
+					var candidates = [];
+					array.forEach(left.occurences, function(leftOccurence, leftIndex) {
+						var rightOccurence = right.occurences[leftIndex];
+						var condition = leftOccurence.file == rightOccurence.file &&
+										leftOccurence.lineIndex == rightOccurence.lineIndex-1;
+						
+						if (condition) {
+							candidates.push(leftOccurence);
+						}
+					});
+					if (candidates.length > 1) {
+						duplicatedBlocks.push({
+							lines: left.lines.concat(right.lines),
+							occurences: candidates//
+						})
+					}
+				}
+			});
+		});
 		
-		return duplications;
+		return duplicatedBlocks;
 	}
 };
 
