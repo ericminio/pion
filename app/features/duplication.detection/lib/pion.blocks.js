f2d0f203caca06295b5c6de5042da313d9f7eec4
ericminio <eric.mignot@gmail.com>
2014-07-28 20:48:11 -0400

    ... ExtractFunction addInCollectionIfNotPresent to reduce Duplication

--- app/features/duplication.detection/lib/pion.blocks.js
+++ app/features/duplication.detection/lib/pion.blocks.js
@@ -4,6 +4,18 @@ var line = require('./line');
 var linesDuplications = require('./pion.lines');
 var theContent = require('./block.detector');
 
+var addInCollectionIfNotPresent = function(collection, item) {
+	itemAlreadyPresent = false;
+	for (var index = 0; index < collection.length; index++) {
+		if (collection[index] == item) {
+			itemAlreadyPresent = true;
+		}
+	}
+	if (! itemAlreadyPresent) {
+		collection.push(item); 
+	}
+};
+
 module.exports = {
 	
 	ignoring: function(patterns) {
@@ -28,54 +40,28 @@ module.exports = {
 		for (var candidateOccurenceIndex = 0; candidateOccurenceIndex < candidate.occurences.length; candidateOccurenceIndex++) {
 			var candidateOccurence = candidate.occurences[candidateOccurenceIndex];
 			
-			if (candidateOccurence.lineIndex == duplicatedLines[1].occurences[candidateOccurenceIndex].lineIndex - 1) { 
-				occurences.push(candidateOccurence); 
+			var matching = candidateOccurence.lineIndex == duplicatedLines[1].occurences[candidateOccurenceIndex].lineIndex - 1;
+			
+			if (matching) { 
 				
-				var lineAlreadyPresent = false;
-				for (var lineIndex = 0; lineIndex < lines.length; lineIndex++) {
-					if (lines[lineIndex] == duplicatedLines[1].lines[0]) {
-						lineAlreadyPresent = true;
-					}
-				}
-				if (! lineAlreadyPresent) {
-					lines.push(duplicatedLines[1].lines[0]);
+				addInCollectionIfNotPresent(occurences, candidateOccurence);
+				addInCollectionIfNotPresent(lines, duplicatedLines[1].lines[0]);
+			}
+			
+			if (duplicatedLines.length > 2) {
+
+				matching = (
+					candidateOccurence.lineIndex == duplicatedLines[1].occurences[candidateOccurenceIndex].lineIndex - 1 &&
+					candidateOccurence.lineIndex == duplicatedLines[2].occurences[candidateOccurenceIndex].lineIndex - 2
+				);
+  
+				if (matching) { 
+					addInCollectionIfNotPresent(occurences, candidateOccurence);
+					addInCollectionIfNotPresent(lines, duplicatedLines[2].lines[0]);				
 				}
 			}
 		}		
 
-		if (duplicatedLines.length > 2) {
-			
-			for (var candidateOccurenceIndex = 0; candidateOccurenceIndex < candidate.occurences.length; candidateOccurenceIndex++) {
-				var candidateOccurence = candidate.occurences[candidateOccurenceIndex];
-			
-				if (candidateOccurence.lineIndex == duplicatedLines[1].occurences[candidateOccurenceIndex].lineIndex - 1 &&
-					candidateOccurence.lineIndex == duplicatedLines[2].occurences[candidateOccurenceIndex].lineIndex - 2) { 
-					
-					occurenceAlreadyPresent = false;
-					for (var occurenceIndex = 0; occurenceIndex < occurences.length; occurenceIndex++) {
-						if (occurences[occurenceIndex] == candidateOccurence) {
-							occurenceAlreadyPresent = true;
-						}
-					}
-					if (! occurenceAlreadyPresent) {
-						occurences.push(candidateOccurence); 
-					}
-									
-					var lineAlreadyPresent = false;
-					for (var lineIndex = 0; lineIndex < lines.length; lineIndex++) {
-						if (lines[lineIndex] == duplicatedLines[2].lines[0]) {
-							lineAlreadyPresent = true;
-						}
-					}
-					if (! lineAlreadyPresent) {
-						lines.push(duplicatedLines[2].lines[0]);
-					}
-				}
-			}	
-		}
-
-	
-		
 		if (occurences.length > 1) {
 			duplicatedBlocks.push({ 
 				lines : lines, 
