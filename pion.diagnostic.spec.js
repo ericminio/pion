79c4446ba5b1ea9ffda9c7830608414bdd0facd6
ericminio <eric.mignot@gmail.com>
2014-08-03 01:03:36 -0400

    Ignore common interface of block and line duplication in Pion's diagnostic

--- pion.diagnostic.spec.js
+++ pion.diagnostic.spec.js
@@ -9,55 +9,57 @@ describe('Running Pion is easy :).', function() {
     describe('Pion', function() {
         
         it('should have 0 duplicated lines', function(done) {       
-			var folder = 'app/';
-			console.log('Scanning directory ' + folder + ' for lines duplication');
-			
+            var folder = 'app/';
+            console.log('Scanning directory ' + folder + ' for lines duplication');
+            
             var fileProvider = require('./app/features/utils/lib/directory.file.provider');
             var detectDuplications = require('./app/features/duplication.detection/lib/pion.lines');
-			detectDuplications.logger = require('./app/features/utils/lib/console.logger');
+            detectDuplications.logger = require('./app/features/utils/lib/console.logger');
             detectDuplications.ignoring(
-				all([javascript, node, jasmine, [
-					
-					'inFiles: function(fileProvider) {',
-					'contentOf: function(filename) {',
-					
-					'occurences: [',
-					'return duplications;',
-					
-					/break/,
-					
-					]])
-            	).inFiles(fileProvider(folder), function(duplications) {
-	            expect(duplications.length).toEqual(0);
-				done();
+                all([javascript, node, jasmine, [
+                    
+                    'ignoring: function(patterns) {',
+                    'this.patterns = patterns;',
+                    'inFiles: function(fileProvider, callback) {',
+                    'contentOf: function(filename) {',
+                    
+                    'occurences: [',
+                    'return duplications;',
+                    
+                    /break/,
+                    
+                    ]])
+                ).inFiles(fileProvider(folder), function(duplications) {
+                expect(duplications.length).toEqual(0);
+                done();
             });
         });
         
         xit('should have 0 duplicated blocks', function(done) {
-			var folder = 'app/';
-			console.log('Scanning directory ' + folder + ' for blocks duplication');
-			
+            var folder = 'app/';
+            console.log('Scanning directory ' + folder + ' for blocks duplication');
+            
             var fileProvider = require('./app/features/utils/lib/directory.file.provider');
             var detectDuplications = require('./app/features/duplication.detection/lib/pion.blocks');
-			detectDuplications.logger = require('./app/features/utils/lib/console.logger');
+            detectDuplications.logger = require('./app/features/utils/lib/console.logger');
             detectDuplications.ignoring(
-				all([javascript, node, jasmine, [
-					
-					'inFiles: function(fileProvider) {',
-					'contentOf: function(filename) {',
-					
-					'occurences: [',
-					'return duplications;',
-					
-					/break/,
-					
-					]])
+                all([javascript, node, jasmine, [
+                    
+                    'inFiles: function(fileProvider) {',
+                    'contentOf: function(filename) {',
+                    
+                    'occurences: [',
+                    'return duplications;',
+                    
+                    /break/,
+                    
+                    ]])
             ).inFiles(fileProvider(folder), function(duplications) {
-				console.log(JSON.stringify(duplications, null, 4));
+                console.log(JSON.stringify(duplications, null, 4));
 
-	            expect(duplications.length).toEqual(0);
-            	done();
-            });			
+                expect(duplications.length).toEqual(0);
+                done();
+            });         
         });
     });
 
